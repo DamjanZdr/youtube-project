89caa13d05dc412025b45a808c6741cc56a1c21b
DamjanZdr <damjan.zdravkovski3@gmail.com>
2026-02-06 14:09:45 +0100

    Update demo video link

--- app/page.tsx
+++ app/page.tsx
@@ -217,7 +217,7 @@ export default async function Home() {
             <div className="relative pt-[56.25%] rounded-xl overflow-hidden">
               <iframe
                 className="absolute inset-0 w-full h-full"
-                src="https://www.youtube.com/embed/4odcTpjDMv0"
+                src="https://www.youtube.com/embed/v2uLDMTNifo"
                 title="Blueprint Demo"
                 allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
                 allowFullScreen
